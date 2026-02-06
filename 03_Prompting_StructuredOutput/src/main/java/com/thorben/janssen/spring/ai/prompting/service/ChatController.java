2bf67173f396abcf7e540848767c818283e92d0a
Thorben Janssen <2053825+thjanssen@users.noreply.github.com>
2026-02-06 19:35:54 +0100

    Cleanup

--- 03_Prompting_StructuredOutput/src/main/java/com/thorben/janssen/spring/ai/prompting/service/ChatController.java
+++ 03_Prompting_StructuredOutput/src/main/java/com/thorben/janssen/spring/ai/prompting/service/ChatController.java
@@ -1,26 +1,14 @@
 package com.thorben.janssen.spring.ai.prompting.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.AdvisorParams;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.ResponseEntity;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.StructuredOutputValidationAdvisor;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.metadata.ChatResponseMetadata;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,7 +29,12 @@ public class ChatController {
     public Flux<String> chat(String message) {
         ClassDescriptionList classDescriptions = chatClient.prompt(message)
                 .advisors(
-                        StructuredOutputValidationAdvisor.builder().outputType(ClassDescription.class).maxRepeatAttempts(1).build(),
+                        // Add StructuredOutputValidationAdvisor for automated validation and retry
+                        StructuredOutputValidationAdvisor.builder()
+                                .outputType(ClassDescriptionList.class)
+                                // optional, default = 3
+                                .maxRepeatAttempts(3)
+                                .build(),
                         SimpleLoggerAdvisor.builder().order(Integer.MAX_VALUE).build()
                 )
                 .call()
